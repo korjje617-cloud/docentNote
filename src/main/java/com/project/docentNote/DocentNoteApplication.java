8ebefd8a55883aaccf0a03b077fe02cf492c192f
korjje617 <korjje617@gmail.com>
2026-02-06 14:53:34 +0900

    초기상태

---
+++ src/main/java/com/project/docentNote/DocentNoteApplication.java
@@ -0,0 +1,13 @@
+package com.project.docentNote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocentNoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocentNoteApplication.class, args);
+	}
+
+}
